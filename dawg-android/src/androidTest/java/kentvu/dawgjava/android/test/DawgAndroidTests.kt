08003c35fc17402cd2c346d3c034e83237893071
Vu Tran Kien <vutrankien.vn@gmail.com>
2020-02-29 19:59:17 +0900

    Remove kotlintest , can't get it executed on android!!

--- dawg-android/src/androidTest/java/kentvu/dawgjava/android/test/DawgAndroidTests.kt
+++ dawg-android/src/androidTest/java/kentvu/dawgjava/android/test/DawgAndroidTests.kt
@@ -1,17 +1,19 @@
-package kentvu.dawgjava.android
+package kentvu.dawgjava.android.test
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import io.kotlintest.shouldBe
-import io.kotlintest.specs.StringSpec
 import kentvu.dawgjava.TrieFactory
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertTrue
+import org.junit.Test
 import org.junit.runner.RunWith
 
 // verify DawgTrie works on Android
 @RunWith(AndroidJUnit4::class)
-class DawgAndroidTest: StringSpec({
-    "buildDawg" {
+class DawgAndroidTest {
+    @Test
+    fun buildDawg() = runBlocking {
         val trie = TrieFactory.newTrie()
         trie.build("a\nb\nc".lineSequence())
-        (trie.contains("a")) shouldBe true
+        assertTrue(trie.contains("a"))
     }
-})
+}
